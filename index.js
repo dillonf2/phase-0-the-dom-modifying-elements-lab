82c13cdf716881ba666cb3fd17d2cd648329a717
Frank Dillon <dillonf2@outlook.com>
2023-02-07 21:27:51 -0500

    modifying dom complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById("main").remove()
+document.createElement("h1");
+const newHeader=document.createElement("h1");
+newHeader.id="victory"
+newHeader.innerHTML="Frank is the champion"
+
